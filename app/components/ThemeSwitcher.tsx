a2ff7c31cac30b60fa0bc04fcfef7afc60fa4318
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2024-12-12 15:36:09 +0530

    Day 9: Added Dark/Light Mode Toggle.

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -4,6 +4,9 @@
 import {useTheme} from "next-themes";
 import {useEffect, useState} from "react";
 
+import {Moon, Sun} from 'lucide-react';
+
+
 export function ThemeSwitcher() {
     const [mounted, setMounted] = useState(false)
     const {theme, setTheme} = useTheme()
@@ -15,10 +18,13 @@ export function ThemeSwitcher() {
     if (!mounted) return null
 
     return (
-        <div>
-            The current theme is: {theme}
-            <button onClick={() => setTheme('light')}>Light Mode</button>
-            <button onClick={() => setTheme('dark')}>Dark Mode</button>
+        <div
+            className={'cursor-pointer bg-neutral-300/50 dark:bg-neutral-700/50 size-10 flex items-center justify-center rounded-xl'}>
+            {theme === 'light' ? (
+                <Moon onClick={() => setTheme('dark')}/>
+            ) : (
+                <Sun className={''} onClick={() => setTheme('light')}/>
+            )}
         </div>
     )
 }
