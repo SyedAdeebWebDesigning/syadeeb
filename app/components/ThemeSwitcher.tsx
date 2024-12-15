db668d7bc5a1c96988cc509d94739d17c266911f
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2024-12-15 19:24:30 +0530

    Day 10: Added 1st Easter Egg .

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -19,14 +19,14 @@ export function ThemeSwitcher() {
 
     return (
         <div
-            className={'w-[84px] h-12 p-1 rounded-full relative bg-neutral-200 text-black cursor-pointer dark:text-white dark:bg-neutral-700'}
+            className={'w-[84px] h-12 p-1 rounded-full relative bg-neutral-200 text-black cursor-pointer dark:text-white dark:bg-neutral-800'}
             onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
             <motion.div
                 initial={false}
 
                 animate={{x: theme === 'light' ? 0 : 35}}
                 transition={{type: "spring", stiffness: 300, damping: 20}}
-                className={`cursor-pointer absolute size-10 bg-neutral-100 dark:bg-neutral-500 flex items-center justify-center rounded-full`}>
+                className={`cursor-pointer absolute size-10 bg-neutral-100 dark:bg-neutral-100/10 flex items-center justify-center rounded-full`}>
                 {theme === 'light' ? (
                     <Sun onClick={() => setTheme('dark')}/>
                 ) : (
