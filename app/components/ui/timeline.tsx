2dd7e4485cc6eeec275c0f8a713aa7736a033b4e
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2024-12-19 23:24:03 +0530

    Day 14: Updated Timeline UI.

--- app/components/ui/timeline.tsx
+++ app/components/ui/timeline.tsx
@@ -44,7 +44,7 @@ export const Timeline = ({data}: { data: TimelineEntry[] }) => {
                         <div
                             className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
                             <div
-                                className="h-10 absolute left-3 md:left-3 w-10 rounded-full flex items-center justify-center">
+                                className="h-10 absolute -left-[11px] md:left-3 w-10 rounded-full flex items-center justify-center">
                                 <div className="h-4 w-4 rounded-full bg-secondaryDark border border-gray-500 p-2"/>
                             </div>
                             <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-700 dark:text-neutral-300">
@@ -52,7 +52,7 @@ export const Timeline = ({data}: { data: TimelineEntry[] }) => {
                             </h3>
                         </div>
                         <div className="relative pl-20 pr-4 md:pl-4 w-full">
-                            <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-300">
+                            <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-700 dark:text-neutral-300">
                                 {item.title}
                             </h3>
                             {item.content}
